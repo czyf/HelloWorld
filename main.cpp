9397f815418d9b39cdf67717c19e5cd2360de867
shiyue <shiyuepz@163.com>
2024-09-07 15:38:49 +0800

    第一次提交

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+void sort(int a[], int n){
+
+}
+
+int main(){
+	int arr[] = {12,4,6,2,3,6,45,74,24,12};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+}
